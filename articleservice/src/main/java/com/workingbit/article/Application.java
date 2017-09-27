9de269910359c02502c6d081afd3624a4ecdda4e
Aleksey <aleksey.popryaduhin@yahoo.com>
2017-09-27 21:22:53 +0300

    return articles resource

--- articleservice/src/main/java/com/workingbit/article/Application.java
+++ articleservice/src/main/java/com/workingbit/article/Application.java
@@ -1,5 +1,6 @@
 package com.workingbit.article;
 
+import com.workingbit.article.article.ArticleController;
 import com.workingbit.article.article.ArticleDao;
 import com.workingbit.article.config.AppProperties;
 import com.workingbit.article.index.IndexController;
@@ -45,8 +46,7 @@ public class Application {
               req.url()));
         });
 
-        get(Path.Web.INDEX, IndexController.serveIndexPage);
-//        get(Path.Web.ARTICLES, ArticleController.fetchAllArticles);
+        get(Path.Web.ARTICLES, ArticleController.fetchAllArticles);
 
         notFound((req, res) -> "Not found");
         internalServerError((req, res) -> "Internal server error");
