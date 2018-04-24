7ffe2ce14df6502a3ad1a22a6c21617a73993f98
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-04-24 12:22:41 +0300

    fix cookie life length

--- sharedmodule/src/main/java/com/workingbit/share/common/RequestConstants.java
+++ sharedmodule/src/main/java/com/workingbit/share/common/RequestConstants.java
@@ -9,7 +9,7 @@ public class RequestConstants {
   public static final String SIGN = "sign";
   public static final String SIGN_REQUEST = "sign-request";
   public static final int SESSION_LENGTH = 20;
-  public static final int COOKIE_AGE = 30 * 24 * 60 * 60;
+  public static final int COOKIE_AGE = 31 * 24 * 60 * 60;
   public static final String ANONYMOUS_SESSION_HEADER = "anonymous-session";
   public static final String USER_SESSION_HEADER = "user-session";
   public static final String USER_ROLE_HEADER = "user-role";
