c6b8024091575baf58a8e4f9cccccd1dbee18140
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-05-13 14:28:15 +0300

    update tables names

--- sharedmodule/src/main/java/com/workingbit/share/common/DBConstants.java
+++ sharedmodule/src/main/java/com/workingbit/share/common/DBConstants.java
@@ -6,10 +6,10 @@ import java.time.format.DateTimeFormatter;
  * Created by Aleksey Popryaduhin on 10:15 13/08/2017.
  */
 public class DBConstants {
-  public static final String BOARD_BOX_TABLE = "SHASHKIWIKI_BoardBox";
-  public static final String ARTICLE_TABLE = "SHASHKIWIKI_Article";
-  public static final String BOARD_TABLE = "SHASHKIWIKI_Board";
-  public static final String NOTATION_TABLE = "SHASHKIWIKI_Notation";
-  public static final String SECURE_USER_TABLE = "SHASHKIWIKI_SecureUser";
+  public static final String BOARD_BOX_TABLE = "SHASHKIWIKIBoardBox";
+  public static final String ARTICLE_TABLE = "SHASHKIWIKIArticle";
+  public static final String BOARD_TABLE = "SHASHKIWIKIBoard";
+  public static final String NOTATION_TABLE = "SHASHKIWIKINotation";
+  public static final String SECURE_USER_TABLE = "SHASHKIWIKISecureUser";
   public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ISO_DATE_TIME;
 }
